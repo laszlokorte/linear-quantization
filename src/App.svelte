cb108d7584dd5f08e14d6751723e6262e405b3c8
Laszlo Korte <git@laszlokorte.de>
2022-07-17 16:42:00 +0200

    add linear signal

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,31 @@
 <script>
-	const formatter = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 })
+	const formatter = new Intl.NumberFormat('en-IN', { maximumFractionDigits: 3, minimumFractionDigits : 2, useGrouping: false })
 	
+	const signalGenerators = {
+		linear: (v, period, amplitude) => Math.sign(amplitude) * (((v/2*period) % amplitude + amplitude)%amplitude * 2 - amplitude),
+		sine: (v, period, amplitude) => amplitude * Math.sin(v*period*Math.PI),
+	}
+
 	let sr = 500
 	let stepBits = 5
 	$: steps = Math.pow(2, stepBits)
 	let bias = 0
 	let period = 1
 	let amplitude = 1
+
+	let signalType = 'linear'
 	
 	$: binSize = 2 / (steps-1)
 	$: bins = Array(steps).fill(0).map((_,b) => -1 + (b+0.5)*binSize - binSize*(bias/2+0.5))
 	
 	
 	$: domain = Array(sr).fill(0).map((_,i,all) => 2*(i/all.length)-1)
-	$: sin = domain.map((v) => amplitude * Math.sin(v*period*Math.PI))
-	$: quan = sin.map(v => bins.reduce((acc, b) => Math.abs(b-v)<Math.abs(acc-v)?b:acc, Infinity))
-	$: err = sin.map((v,i) => Math.abs(quan[i]-sin[i]))
+	$: signal = domain.map((v) => signalGenerators[signalType](v, period, amplitude))
+	$: quan = signal.map(v => bins.reduce((acc, b) => Math.abs(b-v)<=Math.abs(acc-v)?b:acc, Infinity))
+	$: err = signal.map((v,i) => (quan[i]-signal[i]))
 	$: avgErr = err.slice(1).reduce((a,b) => a+b, 0) / err.length
 	
-	$: signalPower = sin.reduce((acc, v) => acc + v*v, 0) / sin.length
+	$: signalPower = signal.reduce((acc, v) => acc + v*v, 0) / signal.length
 	$: errorPower = (err.reduce((acc, v) => acc + v*v, 0) || 1) / err.length
 </script>
 
@@ -39,6 +46,12 @@
 	dt {
 		grid-column: first;
 	}
+
+	footer {
+		margin: 3em;
+		text-align: center;
+	}
+
 </style>
 
 <h1>
@@ -60,33 +73,40 @@
 	
 	
 <dl>
+	<dt>Signal Type</dt>
+	<dd style="display: flex; gap: 2em">
+		{#each Object.keys(signalGenerators) as type}
+		<label style="padding: 0.5em 0"><input type="radio" value={type}  bind:group={signalType} /> {type}</label>
+		{/each}
+	</dd>
+
 	<dt><label for="period">Signal Frequency</label></dt>
 	<dd><input id="period" type="range" min="1" max="10" bind:value={period} /></dd>
-	<dd><output>{period/2}</output></dd>
+	<dd><output>{period}</output></dd>
 	
 	<dt><label for="period">Signal Amplitude</label></dt>
 	<dd><input id="period" type="range" min="-1.5" max="1.5" step="0.01" bind:value={amplitude} /></dd>
-	<dd><output>{amplitude}</output></dd>
+	<dd><output on:dblclick={() => {amplitude = 1}}>{amplitude}</output></dd>
 	
 	<dt><label for="steps">Quantization Bits</label></dt>
 	<dd><input id="steps" type="range" min="1" max="7" bind:value={stepBits} /></dd>
 	<dd><output>{stepBits} bits ({steps} bins)</output></dd>
 	
 	<dt><label for="bias">Quantization Bias</label></dt>
-	<dd><input id="bias" type="range" min="-1" max="1" step="0.01" bind:value={bias} /></dd>
-	<dd><output>{bias}</output></dd>
+	<dd><input id="bias" type="range" min="-5" max="5" step="0.01" bind:value={bias} /></dd>
+	<dd><output on:dblclick={() => {bias = 0}}>{bias}</output></dd>
 	
-	<dt>Signal Power</dt>
-	<dd><output>{formatter.format(signalPower)}</output></dd>
+	<dt style="color: green">Signal Power</dt>
+	<dd style="color: green"><output>{formatter.format(signalPower)}</output></dd>
 	
-	<dt>Error Power</dt>
-	<dd><output>{formatter.format(errorPower)}</output></dd>
+	<dt style="color: red">Error Power</dt>
+	<dd style="color: red"><output>{formatter.format(errorPower)}</output></dd>
 	
 	<dt>Signal/Noise-Ratio (SNR)</dt>
 	<dd><output>{formatter.format(signalPower/errorPower)}</output></dd>
 	
 	<dt>SNR (dB)</dt>
-	<dd><output>{formatter.format(20 * Math.log10(signalPower/errorPower))}</output></dd>
+	<dd><output>{formatter.format(10 * Math.log10(signalPower/errorPower))}</output></dd>
 </dl>
 </fieldset>
 
@@ -111,10 +131,17 @@
 		
 
 	<polyline points="-200,0, {quan.reduce((acc, v, i) => acc + ` ${(domain[i]*200)},${(-v*100)}`,'')} 200 0" stroke-width="0.5" stroke="orange" fill="orange" fill-opacity="0.1" />
-	<polyline points="-200,0 {sin.reduce((acc, v, i) => acc + ` ${(domain[i]*200)},${(-v*100)}`,'')} 200 0" fill="green" stroke-width="1" stroke="green" fill-opacity="0.01" />
+	<polyline points="-200,0 {signal.reduce((acc, v, i) => acc + ` ${(domain[i]*200)},${(-v*100)}`,'')} 200 0" fill="green" stroke-width="1" stroke="green" fill-opacity="0.01" />
 	
 	
 	<polyline points="-200,0, {err.reduce((acc, e, i) => acc + ` ${(domain[i]*200)},${(-e*100)}`,'')} 200 0" stroke-width="0.1" stroke="red" fill="red" fill-opacity="0.3" />
 	
-	<line x1={-200} y1={-avgErr*100} x2={200} y2={-avgErr*100} stroke-width="0.5" stroke-dasharray="2 2" stroke="darkred" />
+	<line x1={200} y1={-Math.sqrt(errorPower)*100} x2={220} y2={-Math.sqrt(errorPower)*100} stroke-width="1.5" stroke="red" />
+	<line x1={200} y1={-Math.sqrt(signalPower)*100} x2={220} y2={-Math.sqrt(signalPower)*100} stroke-width="1.5" stroke="green" />
 </svg>
+
+<footer>
+	<p>
+		<a href="https://tools.laszlokorte.de">more eduational tools</a>
+	</p>
+</footer>
